0d03fdc2ad0f8f690a96c69d3801b46b18c0dc27
meijieli <limeijie@matchyun.com>
2019-02-19 17:57:23 +0800

    init

---
+++ getSwaggerUI.sh
@@ -0,0 +1,16 @@
+mkdir public
+cd public
+wget wget https://raw.githubusercontent.com/swagger-api/swagger-ui/master/dist/favicon-16x16.png
+wget wget https://raw.githubusercontent.com/swagger-api/swagger-ui/master/dist/favicon-32x32.png
+wget wget https://raw.githubusercontent.com/swagger-api/swagger-ui/master/dist/index.html
+wget wget https://raw.githubusercontent.com/swagger-api/swagger-ui/master/dist/oauth2-redirect.html
+wget wget https://raw.githubusercontent.com/swagger-api/swagger-ui/master/dist/swagger-ui-bundle.js
+wget wget https://raw.githubusercontent.com/swagger-api/swagger-ui/master/dist/swagger-ui-bundle.js.map
+wget wget https://raw.githubusercontent.com/swagger-api/swagger-ui/master/dist/swagger-ui-standalone-preset.js
+wget wget https://raw.githubusercontent.com/swagger-api/swagger-ui/master/dist/swagger-ui-standalone-preset.js.map
+wget wget https://raw.githubusercontent.com/swagger-api/swagger-ui/master/dist/swagger-ui.css
+wget wget https://raw.githubusercontent.com/swagger-api/swagger-ui/master/dist/swagger-ui.css.map
+wget wget https://raw.githubusercontent.com/swagger-api/swagger-ui/master/dist/swagger-ui.js
+wget wget https://raw.githubusercontent.com/swagger-api/swagger-ui/master/dist/swagger-ui.js.map
+
+
---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+app.use('/static', express.static('public'));
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function() {
+    console.log('example app listen on port 3000');
+});
